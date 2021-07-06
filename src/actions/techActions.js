4a5c6599a5b7c602af02dd49a46c50f1ce967c12
Abdus-samad <abdussamadajao@gmail.com>
2021-07-06 10:34:49 +0100

    It-logger

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -10,15 +10,16 @@ import {
 export const getTechs = () => async dispatch => {
   try {
     setLoading();
-
+    
     const res = await fetch('/techs');
     const data = await res.json();
-
+    
     dispatch({
       type: GET_TECHS,
       payload: data
     });
   } catch (err) {
+    console.log(err)
     dispatch({
       type: TECHS_ERROR,
       payload: err.response.statusText
